869f836f5b1c0b733808e7d524849d3fb042dc20
ShaLu <sha.lu@csu.fullerton.edu>
2016-04-27 06:52:00 -0700

    add screenshot

--- client/javascripts/app.js
+++ client/javascripts/app.js
@@ -1,85 +1,88 @@
-var main = function (toDoObjects) {
+var main = function(toDoObjects) {
     "use strict";
     console.log("SANITY CHECK");
-    var toDos = toDoObjects.map(function (toDo) {
-          // we'll just return the description(name) of this toDoObject
-          //console.log("toDo.description is: ", toDo.description);
-          return toDo.description;
+    var toDos = toDoObjects.map(function(toDo) {
+        // just return the description(name) of this toDoObject
+        //console.log("toDo.description is: ", toDo.description);
+        return toDo.description;
     });
 
     //= https://openclassrooms.com/courses/ultra-fast-applications-using-node-js/socket-io-let-s-go-to-real-time
     //connected to the server
     var socket = io.connect('http://localhost:3000');
-    
-  // alert all clients receive broadcasting msg
-    socket.on("broadcastData", function(content) {      
+
+    // alert all clients receive broadcasting msg
+    socket.on("broadcastData", function(content) {
         alert("broadcast! A new item was added", content);
         //console.log("broadcast! A new item was added", content);
         // broadcast the update in Newest 
         toDoObjects = content;
         toDos = toDoObjects.map(function(toDo) {
-           return toDo.description;
+            return toDo.description;
         });
 
         var $content;
         // must have empty() to recreate
         $("main .content").empty();
-        if($(".tabs .active").text() === "Newest") {
+        if ($(".tabs .active").text() === "Newest") {
             $content = $("<ul>");
             for (var i = toDos.length - 1; i >= 0; i--) {
                 $content.append($("<li>").text(toDos[i]));
             }
             //$("main .content").append($content);
             // use slidedown need hide() first
-           ($content).hide().appendTo($("main .content")).slideDown(5000);
+            ($content).hide().appendTo($("main .content")).slideDown(5000);
             console.log("newest is: ", toDos);
-        }//end if newest
+        } //end if newest
 
-        else if($(".tabs .active").text() === "Oldest") {
+        else if ($(".tabs .active").text() === "Oldest") {
             $content = $("<ul>");
-            toDos.forEach(function (todo) {
-               $content.append($("<li>").text(todo));
+            toDos.forEach(function(todo) {
+                $content.append($("<li>").text(todo));
             });
             ($content).hide().appendTo($("main .content")).slideDown(5000);
-        }//end else if oldest
-
-        else if($(".tabs .active").text() === "Tags") {
-             var tags = [];
-                toDoObjects.forEach(function (toDo) {
-                    toDo.tags.forEach(function (tag) {
-                        if (tags.indexOf(tag) === -1) {
-                            tags.push(tag);
-                        }
-                    });
+        } //end else if oldest
+        
+        else if ($(".tabs .active").text() === "Tags") {
+            var tags = [];
+            toDoObjects.forEach(function(toDo) {
+                toDo.tags.forEach(function(tag) {
+                    if (tags.indexOf(tag) === -1) {
+                        tags.push(tag);
+                    }
                 });
-                var tagObjects = tags.map(function (tag) {
-                    var toDosWithTag = [];
-                    toDoObjects.forEach(function (toDo) {
-                        if (toDo.tags.indexOf(tag) !== -1) {
-                            toDosWithTag.push(toDo.description);
-                        }
-                    });
-                    return { "name": tag, "toDos": toDosWithTag };
+            });
+            var tagObjects = tags.map(function(tag) {
+                var toDosWithTag = [];
+                toDoObjects.forEach(function(toDo) {
+                    if (toDo.tags.indexOf(tag) !== -1) {
+                        toDosWithTag.push(toDo.description);
+                    }
                 });
-                console.log("tagObjects are: ", tagObjects);
-                tagObjects.forEach(function (tag) {
-                    var $tagName = $("<h3>").text(tag.name),
-                        $content = $("<ul>");
-                    tag.toDos.forEach(function (description) {
-                        var $li = $("<li>").text(description);
-                        $content.append($li);
-                    });
-                    $("main .content").append($tagName);
-                    $("main .content").append($content);
+                return {
+                    "name": tag,
+                    "toDos": toDosWithTag
+                };
+            });
+            console.log("tagObjects are: ", tagObjects);
+            tagObjects.forEach(function(tag) {
+                var $tagName = $("<h3>").text(tag.name),
+                    $content = $("<ul>");
+                tag.toDos.forEach(function(description) {
+                    var $li = $("<li>").text(description);
+                    $content.append($li);
                 });
-        }//end if tags                
-    });// end socket.on
-// ============= when click, update content in each
-    $(".tabs a span").toArray().forEach(function (element) {
+                $("main .content").append($tagName);
+                $("main .content").append($content);
+            });
+        } //end if tags                
+    }); // end socket.on
+    // ============= when click, update content in each
+    $(".tabs a span").toArray().forEach(function(element) {
         var $element = $(element);
 
         // create a click handler for this element
-        $element.on("click", function () {
+        $element.on("click", function() {
             var $content,
                 $input,
                 $button,
@@ -91,24 +94,24 @@ var main = function (toDoObjects) {
             //newest ===================================================
             if ($element.parent().is(":nth-child(1)")) {
                 $content = $("<ul>");
-                for (i = toDos.length-1; i >= 0; i--) {
-                    $content.append($("<li>").text(toDos[i]));   
+                for (i = toDos.length - 1; i >= 0; i--) {
+                    $content.append($("<li>").text(toDos[i]));
                     //console.log("press newest toDos[i] is",toDos[i]);
-                }         
-            } 
+                }
+            }
             //oldest ===================================================
             else if ($element.parent().is(":nth-child(2)")) {
                 $content = $("<ul>");
-                toDos.forEach(function (todo) {
+                toDos.forEach(function(todo) {
                     $content.append($("<li>").text(todo));
                 });
-            } 
+            }
             //tag ======================================================
             else if ($element.parent().is(":nth-child(3)")) {
                 var tags = [];
 
-                toDoObjects.forEach(function (toDo) {
-                    toDo.tags.forEach(function (tag) {
+                toDoObjects.forEach(function(toDo) {
+                    toDo.tags.forEach(function(tag) {
                         if (tags.indexOf(tag) === -1) {
                             tags.push(tag);
                         }
@@ -116,26 +119,29 @@ var main = function (toDoObjects) {
                 });
                 console.log("tags are: ", tags);
 
-                var tagObjects = tags.map(function (tag) {
+                var tagObjects = tags.map(function(tag) {
                     var toDosWithTag = [];
 
-                    toDoObjects.forEach(function (toDo) {
+                    toDoObjects.forEach(function(toDo) {
                         if (toDo.tags.indexOf(tag) !== -1) {
                             toDosWithTag.push(toDo.description);
                         }
                     });
 
-                    return { "name": tag, "toDos": toDosWithTag };
+                    return {
+                        "name": tag,
+                        "toDos": toDosWithTag
+                    };
                 });
 
                 console.log("tagObjects are: ", tagObjects);
 
-                tagObjects.forEach(function (tag) {
+                tagObjects.forEach(function(tag) {
                     var $tagName = $("<h3>").text(tag.name),
                         $content = $("<ul>");
 
 
-                    tag.toDos.forEach(function (description) {
+                    tag.toDos.forEach(function(description) {
                         var $li = $("<li>").text(description);
                         $content.append($li);
                     });
@@ -144,7 +150,7 @@ var main = function (toDoObjects) {
                     $("main .content").append($content);
                 });
 
-            } 
+            }
 
             //Add new todo ==============================================
             else if ($element.parent().is(":nth-child(4)")) {
@@ -154,49 +160,52 @@ var main = function (toDoObjects) {
                     $tagLabel = $("<p>").text("Tags: "),
                     $button = $("<span>").text("+");
 
-                $button.on("click", function () {
+                $button.on("click", function() {
                     var description = $input.val(),
                         tags = $tagInput.val().split(","),
-                        newToDo = {"description":description, "tags":tags};
+                        newToDo = {
+                            "description": description,
+                            "tags": tags
+                        };
 
-                    $.post("todos", newToDo, function (result) {
+                    $.post("todos", newToDo, function(result) {
                         console.log("post todos result: ", result);
 
                         //toDoObjects.push(newToDo);
                         toDoObjects = result;
                         console.log("toDoObjects :", toDoObjects);
                         // update toDos
-                     // ==== socket io emit msg ===============================
-                       socket.emit('clientData', toDoObjects);
-                     // ===================== =================================
-                        toDos = toDoObjects.map(function (toDo) {
+                        // ==== socket io emit msg ===============================
+                        socket.emit('clientData', toDoObjects);
+                        // ===================== =================================
+                        toDos = toDoObjects.map(function(toDo) {
                             return toDo.description;
                         });
 
                         $input.val("");
                         $tagInput.val("");
-                    });// end post
+                    }); // end post
                 }); //end button.on
 
                 $content = $("<div>").append($inputLabel)
-                                     .append($input)
-                                     .append($tagLabel)
-                                     .append($tagInput)
-                                     .append($button);
+                    .append($input)
+                    .append($tagLabel)
+                    .append($tagInput)
+                    .append($button);
             }
 
             $("main .content").append($content);
 
             return false;
-        });// end  $element.on
+        }); // end  $element.on
     });
 
     $(".tabs a:first-child span").trigger("click");
 };
 
-$(document).ready(function () {
-   // var socket = io();
-    $.getJSON("todos.json", function (toDoObjects) {
+$(document).ready(function() {
+    // var socket = io();
+    $.getJSON("todos.json", function(toDoObjects) {
         main(toDoObjects);
     });
 });
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express"),
     app = express(),
     http = require("http").Server(app),
-	  io = require('socket.io')(http),
+    io = require('socket.io')(http),
     // import the mongoose library
     mongoose = require("mongoose");
 
@@ -14,56 +14,58 @@ mongoose.connect('mongodb://localhost/amazeriffic');
 // This is our mongoose model for todos
 var ToDoSchema = mongoose.Schema({
     description: String,
-    tags: [ String ]
+    tags: [String]
 });
 
 var ToDo = mongoose.model("ToDo", ToDoSchema);
 //just http. no createServer
 http.listen(3000, function() {
-	console.log("listening on 3000");
+    console.log("listening on 3000");
 });
 
 // ================  Set socket.io listeners.=================================
 //(sent(emit) from client, need set socket.on linstener on server side to receive)
 io.on('connection', function(socket) {
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
+    console.log('a user connected');
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
+    });
 
-  socket.on('clientData', function(data){
-    console.log("receive client add item: ", data);
-    // broadcast/emit(include itself) the event from the server to all users
-    io.emit('broadcastData', data);
-  });
+    socket.on('clientData', function(data) {
+        console.log("receive client add item: ", data);
+        // broadcast/emit(include itself) the event from the server to all users
+        io.emit('broadcastData', data);
+    });
 });
 // ============================================================================
 
-app.get("/todos.json", function (req, res) {
-    ToDo.find({}, function (err, toDos) {
-	res.json(toDos);
+app.get("/todos.json", function(req, res) {
+    ToDo.find({}, function(err, toDos) {
+        res.json(toDos);
     });
 });
 
-app.post("/todos", function (req, res) {
+app.post("/todos", function(req, res) {
     console.log("req.body is: ", req.body);
-    var newToDo = new ToDo({"description":req.body.description, "tags":req.body.tags});
-    newToDo.save(function (err, result) {
-	if (err !== null) {
-	    // the element did not get saved!
-	    console.log(err);
-	    res.send("ERROR");
-	} else {
-	    // our client expects *all* of the todo items to be returned, so we'll do
-	    // an additional request to maintain compatibility
-	    ToDo.find({}, function (err, result) {
-		if (err !== null) {
-		    // the element did not get saved!
-		    res.send("ERROR");
-		}
-		res.json(result);
-	    });
-	}
+    var newToDo = new ToDo({
+        "description": req.body.description,
+        "tags": req.body.tags
+    });
+    newToDo.save(function(err, result) {
+        if (err !== null) {
+            // the element did not get saved!
+            console.log(err);
+            res.send("ERROR");
+        } else {
+            // our client expects *all* of the todo items to be returned, so we'll do
+            // an additional request to maintain compatibility
+            ToDo.find({}, function(err, result) {
+                if (err !== null) {
+                    // the element did not get saved!
+                    res.send("ERROR");
+                }
+                res.json(result);
+            });
+        }
     });
 });
-
